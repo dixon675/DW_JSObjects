e9f876e69a94c1f33bf1fd15f703343bc4df6afe
Josh Dixon <jdixon675@gmail.com>
2021-03-07 17:05:31 -0700

    added images, troubleshooting tags)

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ function Image(paintPrev, artist, paintTitle, tags, color) {
 	this.artist = artist;
 	this.title = paintTitle
 	this.tags = tags;
-	
 	this.color = color;
 	this.display = function() {
 
 		console.log('entering display function');
 
 		var container = $("<div>")
+
 		this.tags.forEach(function(tag){
 			container.addClass(tag);
 		})
@@ -31,17 +31,18 @@ function Image(paintPrev, artist, paintTitle, tags, color) {
 // Individual images
 
 var paint = [
-	new Image('imgs/starry_night_full.jpg', '~ Vincent Van Gogh ~', 'Starry Night', ["All", "Post-Impressionism", "Van_Gogh"], "#AAE3A7"),
+	new Image('imgs/starry_night_full.jpg', '~ Vincent Van Gogh ~', 'Starry Night', ["All", "Post-Impressionism", "VanGogh"], "#AAE3A7"),
 	new Image('imgs/entry_of_the_crusaders.jpg', '~ Eugene Delacroix ~', 'Entry of the Crusaders in Constantinople', ["All", "Romanticism", "Delacroix"], "#F1C0A0"),
-	new Image('imgs/van_gogh_wheatfield.jpg', '~ Vincent Van Gogh ~', 'Wheatfield with Crows', ["All", "Post-Impressionism", "Van_Gogh"], "#AAE3A7"),
-	new Image('imgs/The_Last_Supper_-_Leonardo_Da_Vinci_-_High_Resolution_32x16.jpg', '~ Leonardo Da Vinci ~', 'The Last Supper', ["All", "Renaissance", "Da_Vinci"], "#F2F1D1"),
-	new Image('imgs/girl_with_pearl_earring.jpg', '~ Johannes Vermeer ~', 'Girl with a Pearl Earring', ["All", "Dutch_Golden_Age", "Vermeer"], "#9BA1D1"),
-	new Image('imgs/Jan_Vermeer_-_The_Art_of_Painting.jpg', '~ Johannes Vermeer ~', 'The Art of Painting', ["All", "Dutch_Golden_Age", "Vermeer"], "#9BA1D1"),
+	new Image('imgs/van_gogh_wheatfield.jpg', '~ Vincent Van Gogh ~', 'Wheatfield with Crows', ["All", "Post-Impressionism", "VanGogh"], "#AAE3A7"),
+	new Image('imgs/The_Last_Supper_-_Leonardo_Da_Vinci_-_High_Resolution_32x16.jpg', '~ Leonardo Da Vinci ~', 'The Last Supper', ["All", "Renaissance", "DaVinci"], "#F2F1D1"),
+	new Image('imgs/girl_with_pearl_earring.jpg', '~ Johannes Vermeer ~', 'Girl with a Pearl Earring', ["All", "DutchGoldenAge", "Vermeer"], "#9BA1D1"),
+	new Image('imgs/Jan_Vermeer_-_The_Art_of_Painting.jpg', '~ Johannes Vermeer ~', 'The Art of Painting', ["All", "DutchGoldenAge", "Vermeer"], "#9BA1D1"),
 	new Image('imgs/libertyLeadingThePeople.jpg', '~ Eugene Delacroix ~', 'Liberty Leading the People', ["All", "Romanticism", "Delacroix"], "#F1C0A0"),
 	new Image('imgs/Eugene_delacroix.jpg', '~ Eugene Delacroix ~', 'Self Portrait', ["All", "Romanticism", "Delacroix"], "#F1C0A0"),
-	new Image('imgs/mona-lisa.jpg', '~ Leonardo Da Vinci ~', 'The Mona Lisa', ["All", "Renaissance", "Da_Vinci"], "#F2F1D1"),
-	new Image('imgs/Vincent_van_Gogh_-_Self-Portrait.jpg', '~ Vincent Van Gogh ~', 'Self Portrait', ["All", "Post-Impressionism", "Van_Gogh"], "#AAE3A7")
-
+	new Image('imgs/mona-lisa.jpg', '~ Leonardo Da Vinci ~', 'The Mona Lisa', ["All", "Renaissance", "DaVinci"], "#F2F1D1"),
+	new Image('imgs/Vincent_van_Gogh_-_Self-Portrait.jpg', '~ Vincent Van Gogh ~', 'Self Portrait', ["All", "Post-Impressionism", "VanGogh"], "#AAE3A7"),
+	new Image('imgs/raftofthemedusa.jpg', '~ Theodore Gericault ~', 'The Raft of the Medusa', ["All", "Romanticism", "Gericault"], '#EEDBF2'),
+	new Image('imgs/Sunday_on_La_Grande_Jatte.jpg', '~ Georges Seurat', 'A Sunday Afternoon on the Island of La Grande Jatte', ['All', 'Post-Impressionism', 'Seurat'], '#D8F3C6')
 ]
 
 
